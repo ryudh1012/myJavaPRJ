69899e5c3ab10aed7d4a3d71e3b990c2b4cdb2d1
ryudh1012 <ryudh1012@gmail.com>
2024-03-26 11:20:10 +0900

    source

---
+++ src/main/java/Tri2.java
@@ -0,0 +1,14 @@
+public class Tri2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < (5+i); j++) {
+                if (j < (4 - i)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
