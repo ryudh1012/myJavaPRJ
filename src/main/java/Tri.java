1f2ac8dd9e7707e00b8c66483437c0d67030d51d
ryudh1012 <ryudh1012@gmail.com>
2024-03-26 11:19:38 +0900

    source

--- src/main/java/Tri.java
+++ src/main/java/Tri.java
@@ -1,13 +1,14 @@
 public class Tri {
-    public static void main(String[] args){
-        for (int i=0; i<4; i++){
-            for(int j=0; j<3-i; j++){
-                System.out.print(" ");
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < (5+i); j++) {
+                if (j < (4 - i)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
             }
-        for (int j=0; j<2*i+1; j++){
-            System.out.print("*");
-        }
-        System.out.println();
+            System.out.println();
         }
     }
 }
