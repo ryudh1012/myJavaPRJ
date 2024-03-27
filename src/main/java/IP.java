25fb90e93db45eec7083d8cd2ef5d09e8948aaa2
ryudh1012 <ryudh1012@gmail.com>
2024-03-27 11:52:19 +0900

    source

--- src/main/java/IP.java
+++ src/main/java/IP.java
@@ -3,11 +3,10 @@ public class IP {
         String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326 \"http://www.example.com/start.html\" \"Mozilla/4.08 [en] (Win98; I ;Nav)\"\n";
 
         System.out.println(log.substring(0,log.indexOf(" -")));
-        System.out.println(log.substring(log.indexOf("\"",2), log.indexOf(" /")));
 
-        int a = log.indexOf(" /");
+        int a = log.indexOf("\"");
         int b = log.indexOf(" /",a);
 
-        System.out.println()
+        System.out.println(log.substring(a+1 ,b));
     }
 }
