954272d62909f729f763f7106649a38e9491baec
ryudh1012 <ryudh1012@gmail.com>
2024-04-09 15:49:13 +0900

    source

---
+++ src/main/java/Code06_06.java
@@ -0,0 +1,12 @@
+public class Code06_06 {
+    public static void main(String [] args){
+        int hap = 0;
+
+        for (int i = 1 ; i <= 10 ; i++) {
+            hap += i;
+
+            System.out.println("hap : " + hap);
+        }
+        System.out.println("1부터 10까지의 합게 : " + hap);
+    }
+}
