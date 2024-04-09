f8a9da43a6c49e1b6a47d2811e42f8ef749cac40
ryudh1012 <ryudh1012@gmail.com>
2024-04-09 13:51:58 +0900

    source

---
+++ src/main/java/Code03_09.java
@@ -0,0 +1,29 @@
+public class Code03_09 {
+    public static void main(String[] args){
+        int num = 20;
+
+        System.out.println(++num + " ");
+
+        System.out.println(num++ + " ");
+        System.out.println(num + " ");
+
+        System.out.print(--num + " ");
+
+        System.out.println(num-- + " ");
+
+        num +=3;
+        System.out.println(num + " ");
+
+        num -=3;
+        System.out.println(num + " ");
+
+        num *=3;
+        System.out.println(num + " ");
+
+        num /=3;
+        System.out.println(num + " ");
+
+        num %=3;
+        System.out.println(num + " ");
+    }
+}
