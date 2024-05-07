fb4385e0057c57fa3655ef048d91e4b377a0f398
ryudh1012 <ryudh1012@gmail.com>
2024-05-07 14:12:04 +0900

    source

---
+++ src/main/java/Code07_06.java
@@ -0,0 +1,14 @@
+class Rabbit {
+    String shape;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
+    public class Code07_06 {
+    public static void main(String[] args){
+    }
+}
