03ce56ff02e2545578d060b3b5cd0020db4ff18d
ryudh1012 <ryudh1012@gmail.com>
2024-05-13 15:35:36 +0900

    source

--- src/main/java/Code08_03.java
+++ src/main/java/Code08_03.java
@@ -17,6 +17,10 @@ class HouseRabbit2 extends Rabbit7 {
     void eatFeed() {
         System.out.println("집토끼가 사료를 먹습니다.");
     }
+    @Override
+    void move(int x, int y) {
+        System.out.println("나는 덮어쓰기한 이동 함수입니다.");
+    }
 }
 
 class MountainRabbit2 extends Rabbit7 {
